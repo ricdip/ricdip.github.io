0132a6309923e2422a75bc4b394d20cd8847f088
ricdip <riccardodpr@gmail.com>
2023-01-08 15:57:22 +0100

    RSS fixes

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -9,7 +9,7 @@ const siteBlogUrl = siteBaseUrl + 'blog/';
 
 export const get = () => rss({
   // <title> field in output XML
-  title: 'ricdip\'s Blog',
+  title: 'Blog | Riccardo Armando Di Prinzio',
   // <description> field in output XML
   description: 'Articles about my work, programming and computer science',
   // base URL for RSS <item> links
@@ -17,6 +17,7 @@ export const get = () => rss({
   // list of <item> in output XML
   items: posts.map((post) => ({
     title: post.frontmatter.title,
+    description: post.frontmatter.excerpt,
     link: siteBlogUrl + post.frontmatter.slug,
     pubDate: markdownDateStringToDate(post.frontmatter.date).toISOString()
   }))
