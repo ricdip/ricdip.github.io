58f10deb3e88a7b733d59c5e53f2edccddbbb5e2
ricdip <riccardodpr@gmail.com>
2023-01-31 23:53:50 +0100

    utils

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,4 +1,10 @@
-export function markdownDateStringToDate(date: string) {
+/**
+ * Function that creates a Date object from a formatted date string
+ * 
+ * @param date Date string in dd-mm-YYYY format
+ * @returns Date object created from date parameter
+ */
+export function markdownDateStringToDate(date: string): Date {
   const [day, month, year] = date.split("-");
   
   return new Date([year, month, day].join("-") + "T00:00:00.000Z");
