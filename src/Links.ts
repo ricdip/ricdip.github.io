d346356038ec03176a7c6f3c82d600e023c1f15e
ricdip <riccardodpr@gmail.com>
2023-01-31 22:13:22 +0100

    refactor

--- src/Links.ts
+++
@@ -1,6 +0,0 @@
-export class Links {
-  static linkedIn: string = "https://www.linkedin.com/in/riccardo-armando-di-prinzio";
-  static gitHub: string = "https://github.com/ricdip";
-  static cvIt: string = "/assets/cv/CV_Riccardo_Armando_Di_Prinzio.pdf";
-  static cvEn: string = "/assets/cv/CV_Riccardo_Armando_Di_Prinzio_en.pdf";
-}
---
+++ src/consts.ts
@@ -0,0 +1,17 @@
+export default class Consts {
+  // index page
+  static indexDescriptionContent: string = "Sito web personale di Riccardo Armando Di Prinzio, informatico.";
+  
+  static indexKeywordsContent: string = "riccardo armando di prinzio, di prinzio, informatica, informatico, software, developer, developing, sviluppatore, java, android, python, mobile, web, javascript, typescript, react native, react, angular, astro";
+
+  // blog page
+  static blogDescriptionContent: string = "Blog personale di Riccardo Armando Di Prinzio. Articoli sul mio lavoro, sulla programmazione e sull'informatica.";
+  
+  static blogKeywordsContent: string = "riccardo armando di prinzio, di prinzio, informatica, informatico, software, developer, developing, sviluppatore, blog";
+
+  // links
+  static linkedInLink: string = "https://www.linkedin.com/in/riccardo-armando-di-prinzio";
+  static gitHubLink: string = "https://github.com/ricdip";
+  static cvItLink: string = "/assets/cv/CV_Riccardo_Armando_Di_Prinzio.pdf";
+  static cvEnLink: string = "/assets/cv/CV_Riccardo_Armando_Di_Prinzio_en.pdf";
+}
